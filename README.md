0c85342edf7c674c76c52c8666052558e8237537
Lucas Machado <machadolusc@gmail.com>
2023-08-30 21:16:19 -0300

    user api call exemple and update readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Projeto para a disciplina de Atividade intercurricular II
 
-- lucas
-- wesley
-- lucas
-- bruno
+here is a [link](https://www.canva.com/design/DAFfwFl740A/yCd27XCaSjTL-nN3hyJASw/edit?utm_content=DAFfwFl740A&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton) to project scope.
+
+**collaborators:**
+
+- Bruno Feiten
+- Lucas Machado
+- Lucas Leal
+- Wesley Santos
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,12 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.json({ name: 'lucas' });
+  return res.json({
+    name: 'dread',
+    age: 24,
+    practiceTime: '5 years',
+    devTechs: ['node', 'python', 'reactjs']
+  });
 });
 
-app.listen(3000, () => console.log('server on port 3000!'));
+app.listen(3000, () => console.log('server on port 3000!\n'));
