5c33c369072b917925cfcdfedfc1a271563bf99f
WesleyGonc <wesleydsg@outlook.com>
2023-09-26 15:17:51 -0300

    new-login-update

--- mobile/components/api.js
+++ mobile/components/api.js
@@ -1,1 +1,28 @@
+;
+// Função para fazer a solicitação de login
+export async function loginUser(email, password) {
+  try {
+    const response = await fetch('http://192.168.20.104/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    if (data.token) {
+        
+      console.log('Login bem-sucedido');
+      return data; 
+    } else {
+      console.error('Erro ao fazer login');
+      return null; 
+    }
+  } catch (error) {
+    console.error('Erro na solicitação de login:', error);
+    return null; 
+  }
+}
 
